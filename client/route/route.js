91453655219d12685aa91045a5f2c889cb0dff5e
C. Thomas Bailey <christopher.thomas.bailey@gmail.com>
2016-11-19 16:38:32 -0500

    Clean up Google Maps code

--- client/route/route.js
+++ client/route/route.js
@@ -5,36 +5,36 @@ var destination = defaultDestination
 var directionsService
 var directionsDisplay
 
-//function updateDistance () {
-  //Meteor.call('getDistance', {
-    //origin,
-    //destination
-  //}, (err, distance) => {
-    //if (err) {
-      //console.error(`Error when getting distance: ${err}`)
-    //} else {
-      //Session.set('distance', distance)
-    //}
-  //})
-//}
+function toMiles (meters) {
+  const inches = meters * 39.370
+  return inches / 63360
+}
 
-//function updateMapSrc () {
-  //Meteor.call('getMapSrc', {
-    //origin,
-    //destination
-  //}, (err, url) => {
-    //if (err) {
-      //console.error(`Error when getting map src: ${err}`)
-    //} else {
-      //Session.set('mapSrc', url)
-    //}
-  //})
-//}
+function updateRoute() {
+  directionsService.route({
+    origin,
+    destination,
+    travelMode: 'DRIVING'
+  }, (response, status) => {
+    if (status === 'OK') {
+      const legs = response.routes[0].legs
+      const sum = (total, n) => total + n
+      const totalMeters = legs.map(leg => leg.distance.value).reduce(sum)
+      Session.set('distance', toMiles(totalMeters))
+      directionsDisplay.setDirections(response)
+    } else {
+      window.alert('Directions request failed due to ' + status)
+    }
+  })
+}
 
-//function updateMap () {
-  //updateMapSrc()
-  //updateDistance()
-//}
+window.initMap = function () {
+  directionsService = new google.maps.DirectionsService
+  directionsDisplay = new google.maps.DirectionsRenderer
+  const map = new google.maps.Map(document.getElementById('map'))
+  directionsDisplay.setMap(map)
+  updateRoute()
+}
 
 Template.route.helpers({
   distance () {
@@ -42,9 +42,6 @@ Template.route.helpers({
     if (distance) {
       return distance.toFixed(1)
     }
-  },
-  mapSrc () {
-    return Session.get('mapSrc')
   },
   defaultOrigin () {
     return defaultOrigin
@@ -54,35 +51,6 @@ Template.route.helpers({
   }
 })
 
-function calculateAndDisplayRoute() {
-  directionsService.route({
-    origin: origin,
-    destination: destination,
-    travelMode: 'DRIVING'
-  }, function (response, status) {
-    if (status === 'OK') {
-      const legs = response.routes[0].legs
-      const sum = (total, n) => total + n
-      const totalMeters = legs.map(leg => leg.distance.value).reduce(sum)
-      const totalInches = totalMeters * 39.370
-      const totalMiles = totalInches / 63360
-      Session.set('distance', totalMiles)
-      directionsDisplay.setDirections(response)
-    } else {
-      window.alert('Directions request failed due to ' + status)
-    }
-  })
-}
-
-window.initMap = function () {
-  console.log('initing map')
-  directionsService = new google.maps.DirectionsService
-  directionsDisplay = new google.maps.DirectionsRenderer
-  var map = new google.maps.Map(document.getElementById('map'))
-  directionsDisplay.setMap(map)
-  calculateAndDisplayRoute()
-}
-
 Template.route.events({
   'input #origin': (e) => {
     origin = encodeURIComponent(e.target.value)
@@ -91,15 +59,11 @@ Template.route.events({
     destination = encodeURIComponent(e.target.value)
   },
   'click #update-map': () => {
-    calculateAndDisplayRoute()
+    updateRoute()
   }
 })
 
-//Template.route.onCreated(() => {
-  //updateMap()
-//})
-
-Template.route.onRendered(() => {
+Template.route.onCreated(() => {
   const apiKey = 'AIzaSyDPLvgIuiBMSj5wpOM3RDwxEr5UW9i0H3U'
   const callbackName = 'initMap'
   const googleMapsApiSrc = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=${callbackName}`
